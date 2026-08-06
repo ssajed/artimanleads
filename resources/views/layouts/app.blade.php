1c6aa30a6c4f78575e507933f63b0b3d470aa7e3
ssajed <sajed1003@gmail.com>
2026-08-06 07:08:46 -0400

    fix: load local Vite assets in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <title>{{ config('app.name', 'ArtimanLeads') }}</title>
 
     <!-- Fonts - Vazirmatn -->
